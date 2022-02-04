826fa47be27fbd6fb0e0397b311afee0ed4740eb
Angela <angelabecerraforero@gmail.com>
2022-02-03 20:59:21 -0500

    Subir cambios a GitHub

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,73 @@
 const cipher = {
-  // ...
-};
+  encode, decode 
+}
+// Recibe los valores del input
+function encode(saltos, cifrado) {
+  console.log(typeof saltos);
+  // Validar datos
+  if (typeof cifrado !== 'string' || cifrado.length === 0){
+    throw new TypeError("Por favor ingresa un texto");
+  }
+  if (typeof saltos !== 'number' || saltos.length === 0){
+    throw new TypeError("Por favor elige un número");
+  } console.log("Falta Número")
+  // variable que almacenará el cifrado
+  let resultado = "";
+  // abecedario que se va a utilizar
+  let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Sacar el residuo a los saltos en caso de ser negativo
+  saltos = ((saltos % 26) + 26) % 26;
 
+  if (cifrado) {
+    // Cuenta la longitud del mensaje de Cifrado
+    for (let i = 0; i < cifrado.length; i++) {
+      // Verifica que la letra se encuentre en el abecedario
+      if (abecedario.indexOf(cifrado[i]) != -1) {
+        // Variable que guarda la posición de la letra
+        // Le suma la letra encontrada a los Saltos para después sacarle el residuo
+        let posicion = (abecedario.indexOf(cifrado[i]) + saltos) % 26;
+        // Concatenar al resultado
+        resultado += abecedario[posicion];
+      } else {
+        resultado += cifrado[i];
+      }
+    }
+  }
+  console.log(resultado)
+  return resultado;
+}
+//Recibe los valores del input
+function decode(saltos, cifrado) {
+  // Validar datos
+  if (typeof cifrado !== 'string' || cifrado.length === 0){
+    throw TypeError("Por favor ingresa un texto");
+  }
+  if (typeof saltos !== 'number' || saltos.length === 0){
+    throw TypeError("Por favor elige un número")
+  }
+  //Variable que elmacenará el descifrado
+  let resultadoDes = "";
+  //Abecedario
+  let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //Sacar el residuo
+  saltos = ((saltos % 26) - 26) % 26;
+
+  if (cifrado) {
+    // Cuenta la longitud del mensaje de Cifrado
+    for (let i = 0; i < cifrado.length; i++) {
+      // Verifica que la letra se encuentre en el abecedario
+      if (abecedario.indexOf(cifrado[i]) != -1) {
+        // Variable que guarda la posición de la letra
+        // Le suma la letra encontrada a los Saltos para después sacarle el residuo
+        let posicion = (abecedario.indexOf(cifrado[i]) - saltos) % 26;
+        // Concatenar al resultado
+        resultadoDes += abecedario[posicion];
+      } else {
+        resultadoDes += cifrado[i];
+      }
+    }
+  }
+  console.log(resultadoDes)
+  return resultadoDes;
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function cambiarAMayuscula(){
+    document.getElementById("texto1").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    });
+}
+cambiarAMayuscula()
+
+const btnCifrar = document.getElementById("cifrar");
+
+btnCifrar.addEventListener("click", () => {
+        let cifrado = document.getElementById("texto1").value;
+        let saltosStr = document.getElementById("saltos").value;
+        let saltos = parseInt(saltosStr);
+        console.log(parseInt(""));
+        document.getElementById("texto2").value = cipher.encode(saltos, cifrado);
+    }
+);
+
+const btnDesCifrar = document.getElementById("descifrar");
+
+btnDesCifrar.addEventListener("click", () => {
+    console.log("Se hizo click");
+        let cifrado = document.getElementById("texto1").value;
+        let saltosStr = document.getElementById("saltos").value;
+        let saltos = parseInt(saltosStr);
+        console.log(saltos);
+        document.getElementById("texto2").value = cipher.decode(saltos, cifrado);  
+    }
+); 
